ffc8a9a500c3f67a55e44fab36ee83abf613f304
Bart <bart@myotis>
2025-05-12 12:38:17 +0200

    Allow failure in test

--- tests/testthat/test-get_pvol_ee.R
+++ tests/testthat/test-get_pvol_ee.R
@@ -2,11 +2,13 @@ test_that("Pvol for estonia can be downloaded", {
   skip_if_offline()
   withr::local_options(list(httr2_progress = FALSE))
   # The api frequently sends a 429 response therefore test is allowed to fail
-  pvol <- expect_no_error(get_pvol("eesur",
+  show_failure(expect_no_error(pvol <- get_pvol("eesur",
     time <- as.POSIXct("2024-4-4 21:00:00", tz = "Europe/Helsinki"),
     param = "all"
-  ))
-  skip_if_not(exists("pvol"))
+  )))
+  skip_if_not(inherits(pvol, "pvol"),
+    message = "PVOL download for estonia was unsuccesful, succes is variable in testing environments"
+  )
   expect_s3_class(pvol, "pvol")
   expect_true(bioRad::is.pvol(pvol))
   expect_identical(pvol$datetime, lubridate::with_tz(time, "UTC"))

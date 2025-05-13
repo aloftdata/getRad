13d55f5565d03324f9f2a01e5b3574b8d9c78c8c
PietrH <pieterhuy@gmail.com>
2025-05-13 12:30:05 +0200

    Add tests for extracting single values from a line.

--- tests/testthat/test-utils-rmi.R
+++ tests/testthat/test-utils-rmi.R
@@ -7,3 +7,120 @@ test_that("get_datetime() returns the expected value", {
     lubridate::ymd("2020-01-24", tz = "UTC")
   )
 })
+
+test_that("get_height() returns the expected value", {
+  expect_identical(
+    get_height(fwf_text[6]),
+    200L
+  )
+})
+
+test_that("get_u() returns the expected value", {
+  expect_identical(
+    get_u(fwf_text[6]),
+    0.58
+  )
+})
+
+test_that("get_v() returns the expected value", {
+  expect_identical(
+    get_v(fwf_text[6]),
+    1.29
+  )
+})
+
+test_that("get_w() returns the expected value", {
+  expect_identical(
+    get_w(fwf_text[6]),
+    6.25
+  )
+})
+
+test_that("get_ff() returns the expected value", {
+  expect_identical(
+    get_ff(fwf_text[6]),
+    1.42
+  )
+})
+
+test_that("get_dd() returns the expected value", {
+  expect_identical(
+    get_dd(fwf_text[6]),
+    24.3
+  )
+})
+
+test_that("get_sd_vpp() returns the expected value", {
+  expect_identical(
+    get_sd_vpp(fwf_text[6]),
+    1.49
+  )
+})
+
+test_that("get_gap() returns the expected value", {
+  expect_identical(
+    get_gap(fwf_text[6]),
+    FALSE
+  )
+})
+
+test_that("get_dbz() returns the expected value", {
+  expect_identical(
+    get_dbz(fwf_text[6]),
+    -10.38
+  )
+})
+
+test_that("get_eta() returns the expected value", {
+  expect_identical(
+    get_eta(fwf_text[6]),
+    32.2
+  )
+})
+
+test_that("get_dens() returns the expected value", {
+  expect_identical(
+    get_dens(fwf_text[6]),
+    0
+  )
+
+  expect_identical(
+    get_dens(fwf_text[3680]),
+    11.10
+  )
+})
+
+test_that("get_dbzh() returns the expected value", {
+  expect_identical(
+    get_dbzh(fwf_text[6]),
+    26.95
+  )
+})
+
+test_that("get_n() returns the expected value", {
+  expect_identical(
+    get_n(fwf_text[6]),
+    184L
+  )
+})
+
+test_that("get_n_dbz() returns the expected value", {
+  expect_identical(
+    get_n_dbz(fwf_text[6]),
+    829L
+  )
+})
+
+test_that("get_n_all() returns the expected value", {
+  expect_identical(
+    get_n_all(fwf_text[6]),
+    1593L
+  )
+})
+
+test_that("get_n_dbz_all() returns the expected value", {
+  expect_identical(
+    get_n_dbz_all(fwf_text[6]),
+    3233L
+  )
+})

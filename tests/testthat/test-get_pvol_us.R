886636e712846e82f3da4bcba4f82a3f1cb225e0
Bart <bart@myotis>
2025-05-14 09:28:03 +0200

    shorten test interval

--- tests/testthat/test-get_pvol_us.R
+++ tests/testthat/test-get_pvol_us.R
@@ -1,5 +1,5 @@
 time_utc <- lubridate::floor_date(Sys.time() - lubridate::hours(12), "5 mins")
-dt_int   <- lubridate::interval(time_utc, time_utc + lubridate::minutes(30))
+dt_int   <- lubridate::interval(time_utc, time_utc + lubridate::minutes(10))
 
 test_that("NEXRAD polar volume can be downloaded", {
   skip_if_offline(host = "noaa-nexrad-level2.s3.amazonaws.com")
@@ -10,15 +10,15 @@ test_that("NEXRAD polar volume can be downloaded", {
 })
 
 test_that("Mixed radar vector (single timestamp)", {
-  skip_if_offline() 
+  skip_if_offline()
   pvols <- getRad::get_pvol(c("KABR", "czska"), time_utc)
   expect_true(is.list(pvols))
   expect_gt(length(pvols), 0)
   expect_true(all(purrr::map_lgl(pvols, ~ inherits(.x, "pvol"))))
 })
 
-test_that("Mixed radar vector + 1-hour interval", {
-  skip_if_offline() 
+test_that("Mixed radar vector + 10 minute interval", {
+  skip_if_offline()
   pvols <- getRad::get_pvol(c("KABR", "czska"), dt_int)
   expect_true(is.list(pvols))
   expect_gt(length(pvols), 0)

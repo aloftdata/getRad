d26341f82c3aeea0d00c7eaed6c0f5907b5f0283
Bart <bart@myotis>
2025-07-17 22:37:49 +0200

    Make sure skipped if offline

--- tests/testthat/test-get_pvol_se.R
+++ tests/testthat/test-get_pvol_se.R
@@ -1,4 +1,5 @@
 test_that("Pvol for Sweden can be downloaded", {
+  skip_if_offline("opendata-download-radar.smhi.se")
   time <- as.POSIXct(Sys.time(), tz = "Europe/Helsinki") - lubridate::hours(10)
   expect_s3_class(pvol <- get_pvol("seatv", time, param = "all"), "pvol")
   expect_true(bioRad::is.pvol(pvol))
@@ -9,6 +10,7 @@ test_that("Pvol for Sweden can be downloaded", {
 })
 
 test_that("Pvol for Sweden fails out of time range", {
+  skip_if_offline("opendata-download-radar.smhi.se")
   time <- Sys.time() - lubridate::hours(40)
   expect_error(get_pvol("sehuv", time), class = "getRad_error_get_pvol_se_data_not_found")
   time <- Sys.time() + lubridate::hours(1)
@@ -16,6 +18,7 @@ test_that("Pvol for Sweden fails out of time range", {
 })
 
 test_that("Pvol for Sweden fails incorrect radar", {
+  skip_if_offline("opendata-download-radar.smhi.se")
   time <- Sys.time() - lubridate::hours(10)
   expect_error(get_pvol("sehut", time), class = "getRad_error_get_pvol_se_radar_not_found")
 })

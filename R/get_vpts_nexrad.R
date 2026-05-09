54c79a127051d9bf905f67e15e5795ceca6de531
iskandari <sasha.tedeschi@gmail.com>
2026-05-08 23:59:30 -0400

    change get-vpts_nexrad to only read daily not to gunzip

--- R/get_vpts_nexrad.R
+++ R/get_vpts_nexrad.R
@@ -4,17 +4,13 @@
 #'
 #' @details
 #' By default, data are retrieved from the public BirdCast S3 archive at
-#' `https://birdcastdata.s3.amazonaws.com/nexrad`.
+#' `https://birdcastdata.s3.amazonaws.com/nexrad/daily`.
 #'
-#' The default path format is monthly:
-#' `"{radar}/{year}/{radar}_vpts_{year}{month}.csv.gz"`.
-#'
-#' To read daily files, set:
-#' `options(getRad.vpts_nexrad_path_format =
-#' "{radar}/{year}/{radar}_vpts_{year}{month}{day}.csv")`.
+#' The expected path format is:
+#' `"{radar}/{year}/{radar}_vpts_{year}{month}{day}.csv"`.
 #'
 #' @section Inner working:
-#' - Constructs the S3 paths for the VPTS files based on the input.
+#' - Constructs the S3 paths for the daily VPTS files based on the input.
 #' - Performs parallel HTTP requests to fetch the VPTS CSV data.
 #' - Parses the response bodies with the shared VPTS column classes.
 #' - Uses uppercase NEXRAD radar codes for archive paths.
@@ -27,46 +23,30 @@
 get_vpts_nexrad <- function(radar, rounded_interval) {
   radar <- toupper(radar)
 
-  path_format <- getOption(
-    "getRad.vpts_nexrad_path_format",
-    "{radar}/{year}/{radar}_vpts_{year}{month}.csv.gz"
-  )
-
-  prefix <- if (grepl("\\{day\\}", path_format)) {
-    "daily"
-  } else {
-    "monthly"
-  }
-
-  base_url <- file.path(
-    "https://birdcastdata.s3.amazonaws.com/nexrad",
-    prefix
-  )
-
   dates <- seq(
     lubridate::as_date(lubridate::int_start(rounded_interval)),
     lubridate::as_date(lubridate::int_end(rounded_interval)),
     by = "day"
   )
 
-  paths <- purrr::map_chr(
+  s3_paths <- purrr::map_chr(
     dates,
     \(date) {
       glue::glue(
-        path_format,
+        "{radar}/{year}/{radar}_vpts_{year}{month}{day}.csv",
         radar = radar,
         year = lubridate::year(date),
         month = sprintf("%02d", lubridate::month(date)),
-        day = sprintf("%02d", lubridate::day(date)),
-        date = date
+        day = sprintf("%02d", lubridate::day(date))
       )
     }
   )
 
-  urls <- unique(file.path(base_url, paths))
+  base_url <- "https://birdcastdata.s3.amazonaws.com/nexrad/daily"
 
   out <-
-    read_vpts_from_url(urls) |>
+    paste(base_url, s3_paths, sep = "/") |>
+    read_vpts_from_url() |>
     purrr::keep(.p = ~ as.logical(nrow(.x))) |>
     purrr::list_rbind()
 

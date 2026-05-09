0a8c9e08abf20b9c362574c46e71a73b5a74dfb4
iskandari <sasha.tedeschi@gmail.com>
2026-05-08 23:59:30 -0400

    change get-vpts_nexrad to only read daily not to gunzip

--- tests/testthat/test-get_vpts_nexrad.R
+++ tests/testthat/test-get_vpts_nexrad.R
@@ -1,14 +1,9 @@
-test_that("get_vpts_nexrad() can fetch monthly VPTS data from BirdCast archive", {
+test_that("get_vpts_nexrad() can fetch daily VPTS data from BirdCast archive", {
   skip_if_offline()
 
-  nexrad_vpts_tbl <- get_vpts_nexrad(
+  nexrad_vpts_tbl <- getRad:::get_vpts_nexrad(
     radar = "KABR",
     rounded_interval = lubridate::interval("2013-09-01", "2013-09-02")
-  )
-
-  expect_type(
-    nexrad_vpts_tbl,
-    "list"
   )
 
   expect_s3_class(
@@ -19,9 +14,10 @@ test_that("get_vpts_nexrad() can fetch monthly VPTS data from BirdCast archive",
   expect_named(
     nexrad_vpts_tbl,
     c(
-      "datetime",
       "radar",
+      "datetime",
       "height",
+      "height_reference",
       "u",
       "v",
       "w",
@@ -49,6 +45,7 @@ test_that("get_vpts_nexrad() can fetch monthly VPTS data from BirdCast archive",
     )
   )
 
+  expect_true(nrow(nexrad_vpts_tbl) > 0)
   expect_true(all(nexrad_vpts_tbl$radar == "kabr"))
   expect_true(all(nexrad_vpts_tbl$source == "nexrad"))
 })

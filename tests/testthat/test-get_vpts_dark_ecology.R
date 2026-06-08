727c4be3c2b7a10c03298696deb16af0f3ddd476
PietrH <pieterhuy@gmail.com>
2026-06-08 11:05:06 +0200

    Add test for vp return

--- tests/testthat/test-get_vpts_dark_ecology.R
+++ tests/testthat/test-get_vpts_dark_ecology.R
@@ -1,6 +1,27 @@
 test_that("get_vpts_dark_ecology() returns error on invalid odim code", {})
 
-test_that("get_vpts_dark_ecology() can read dark ecology data from disk", {})
+test_that("get_vpts_dark_ecology() can read dark ecology data from disk", {
+  dk_path <- system.file("extdata", "darkecology", package = "getRad")
+
+  expect_type(
+    kcbx_vpts <- get_vpts_dark_ecology(
+      directory = dk_path,
+      radar = "KCBX",
+      rounded_interval = lubridate::interval(
+        start = "20150101",
+        end = "20150201"
+      )
+    ),
+    "list"
+  )
+
+  purrr::map(
+    kcbx_vpts,
+    \(list_child) {
+      expect_s3_class(list_child, "vp")
+    }
+  )
+})
 
 test_that("get_vpts_dark_ecology() supports reading multiple radars", {})
 

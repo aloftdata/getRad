3d5dc5107143e564cac61dabac762fc8079feee6
PietrH <pieterhuy@gmail.com>
2026-06-08 10:43:09 +0200

    Stub tests

---
+++ tests/testthat/test-get_vpts_dark_ecology.R
@@ -0,0 +1,9 @@
+test_that("get_vpts_dark_ecology() returns error on invalid odim code", {})
+
+test_that("get_vpts_dark_ecology() can read dark ecology data from disk", {})
+
+test_that("get_vpts_dark_ecology() supports reading multiple radars", {})
+
+test_that("get_vpts_aloft() returns error when radar is not found", {})
+
+test_that("get_vpts_dark_ecology() returns error when date is not found", {})

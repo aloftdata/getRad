bca5c0b8d65bd40b6c3b0c3a63299e409deee3df
PietrH <pieterhuy@gmail.com>
2026-06-08 10:27:24 +0200

    Refactor to only use directory scanning, not vroom

--- R/get_vpts_dark_ecology.R
+++ R/get_vpts_dark_ecology.R
@@ -1,8 +1,10 @@
-#' Read dark ecology vpts profiles from disk via arrow
+#' Read dark ecology vpts profiles from disk
 #'
 #' @param path
 #' @param radar
 #' @param rounded_interval
+#' @param ... additional arguments passed to `bioRad::read_cajun()`
+#' @param call
 #'
 #' @returns
 #'
@@ -13,57 +15,6 @@
 #' )
 
 read_vpts_dark_ecology <- function(
-  radar,
-  rounded_interval,
-  path = "/media/pieter_huybrechts/data/profiles_lite/",
-  ...,
-  call = rlang::caller_env()
-) {
-  part <- arrow::DirectoryPartitioning$create(
-    arrow::schema(
-      year = arrow::int32(),
-      month = arrow::int32(),
-      day = arrow::int32(),
-      station = arrow::utf8()
-    )
-  )
-
-  profiles <- arrow::open_csv_dataset(path, partitioning = part) |>
-    dplyr::mutate(
-      filename = arrow::add_filename(),
-      date = lubridate::ymd(paste(year, month, day))
-    ) |>
-    # required to create the filenames
-    dplyr::collect() |>
-    dplyr::mutate(
-      datetime = lubridate::ymd_hms(paste(
-        date,
-        stringr::str_extract(filename, "[0-9]+(?=\\.)")
-      ))
-    ) |>
-    dplyr::filter(
-      .data$station == radar,
-      .data$datetime %within% rounded_interval
-    )
-
-  profiles
-}
-
-#' Read dark ecology vpts profiles from disk via vroom and guessing paths
-#'
-#' @param path
-#' @param radar
-#' @param rounded_interval
-#' @param ... additional arguments passed to `bioRad::read_cajun()`
-#'
-#' @returns
-#'
-#' @examples
-#' read_vpts_dark_ecology2(
-#'   radar = "KCBX",
-#'   rounded_interval = lubridate::interval(start = "20150101", end = "20150201")
-#' )
-read_vpts_dark_ecology2 <- function(
   path = "/media/pieter_huybrechts/data/profiles_lite/",
   radar,
   rounded_interval,

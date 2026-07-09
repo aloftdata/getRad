b28e9d785b7fc14d4db441d5a4f58fb7dbb8a082
bart1 <1662852+bart1@users.noreply.github.com>
2026-07-09 12:17:57 +0200

    Change getRad.progress to be interactive so it is silent in batch settings

--- R/zzz.R
+++ R/zzz.R
@@ -15,7 +15,7 @@
   op <- options()
   op.getRad <- list(
     getRad.key_prefix = "getRad_",
-    getRad.progress = TRUE,
+    getRad.progress = interactive(),
     getRad.user_agent = paste(
       "R package getRad",
       getNamespaceVersion("getRad")

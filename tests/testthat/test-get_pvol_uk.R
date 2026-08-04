326b10fb8465468138293b8b7e92f6f06ac2b610
Bart <you@example.com>
2026-08-04 16:21:36 +0200

    format code

--- tests/testthat/test-get_pvol_uk.R
+++ tests/testthat/test-get_pvol_uk.R
@@ -8,7 +8,10 @@ test_that("Pvol for uk can be downloaded", {
     lubridate::floor_date(pvol$datetime, "5 mins"),
     lubridate::with_tz(time, "UTC")
   )
-  pvolsp <- expect_s3_class(get_pvol("ukjer", pulse_type="sp", time, param = "all"), "pvol")
+  pvolsp <- expect_s3_class(
+    get_pvol("ukjer", pulse_type = "sp", time, param = "all"),
+    "pvol"
+  )
   expect_true(bioRad::is.pvol(pvolsp))
   expect_identical(
     lubridate::floor_date(pvolsp$datetime, "5 mins"),
@@ -17,20 +20,24 @@ test_that("Pvol for uk can be downloaded", {
   expect_false(identical(pvol, pvolsp))
 })
 
-test_that("plusetype argument",{
-  expect_error(get_pvol("ukcas", Sys.time(), pulse_type="ll"),"pulse_type")
-  expect_error(get_pvol("ukcas", Sys.time(), pulse_type=1),"pulse_type")
+test_that("plusetype argument", {
+  expect_error(get_pvol("ukcas", Sys.time(), pulse_type = "ll"), "pulse_type")
+  expect_error(get_pvol("ukcas", Sys.time(), pulse_type = 1), "pulse_type")
 })
-test_that("404 no data",{
+test_that("404 no data", {
   skip_if_offline("ncas-radar-o.s3-ext.jc.rl.ac.uk")
-  expect_error(get_pvol("ukcas", as.POSIXct("2012-4-1 10:00:10")),class="getRad_error_uk_no_data_404")
+  expect_error(
+    get_pvol("ukcas", as.POSIXct("2012-4-1 10:00:10")),
+    class = "getRad_error_uk_no_data_404"
+  )
 })
 
-test_that("no sp data",{
+test_that("no sp data", {
   skip_if_offline("ncas-radar-o.s3-ext.jc.rl.ac.uk")
   time <-
     as.POSIXct("2022-4-1 10:05:10", tz = "Europe/Helsinki")
-  expect_error(get_pvol("ukcas", time, pulse_type="sp"),class="getRad_error_uk_no_sp_data")
+  expect_error(
+    get_pvol("ukcas", time, pulse_type = "sp"),
+    class = "getRad_error_uk_no_sp_data"
+  )
 })
-
-

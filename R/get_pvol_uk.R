01e7c5a0ef981db6dc4e6c011750e488b3b4ce6a
Bart <you@example.com>
2026-08-04 16:20:12 +0200

    add error messaging and tests

--- R/get_pvol_uk.R
+++ R/get_pvol_uk.R
@@ -31,14 +31,27 @@ get_pvol_uk <- function(radar, time, pulse_type=c("lp", "sp"), ..., call = rlang
                  ukthu = 20,
                  ukmun = 19,
                  ukhmy = 23)
-  pulse_type<- rlang::arg_match(pulse_type)
-  # TODO wardon-hill code 11 no odim coe
+  pulse_type<- rlang::arg_match(pulse_type, error_call = call)
+  time<-lubridate::with_tz(time, "UTC")
+  # TODO wardon-hill code 11 no odim code
+  if(pulse_type=="sp" && time !=lubridate::floor_date(time,"10 mins")){
+    cli::cli_abort(call=call, c(x="Short pulse data is only available every ten minutes.", i="To resolve round the {.arg time} to the nearest 10 minutes."), class="getRad_error_uk_no_sp_data")
+  }
   withr::with_file("file.h5", {
-    req <- httr2::request(glue::glue(
+    url<-glue::glue(
       "https://ncas-radar-o.s3-ext.jc.rl.ac.uk/uk-wsr-visualizer-public/ukmo-nimrod/pvol/{uk_odim_map[radar]}/{format(time,'%Y/%m/%d')}/{pulse_type}/{format(time,'%Y%m%d')}_polar_pl_radar{sprintf('%02d', uk_odim_map_radar_number[radar])}_aggregate_{pulse_type}_{format(time,'%H%M')}.h5"
-    )) |>
+    )
+    req <- tryCatch(httr2::request(url) |>
       req_user_agent_getrad() |>
-      httr2::req_perform(path = "file.h5", error_call = call)
+      httr2::req_perform(path = "file.h5", error_call = call),
+    httr2_http_404 = function(cnd) {
+      cli::cli_abort(c(x="No data was found for this radar ({.val {radar}}) at the specified search time ({.val {time}})", i="The url constructed was: {.url {url}}."),
+        call = call,
+        cnd = cnd,
+        class = "getRad_error_uk_no_data_404"
+      )
+    }
+  )
     pvol <- bioRad::read_pvolfile(req$body, ...)
   })
   return(pvol)
---
+++ tests/testthat/test-get_pvol_uk.R
@@ -0,0 +1,36 @@
+test_that("Pvol for uk can be downloaded", {
+  skip_if_offline("ncas-radar-o.s3-ext.jc.rl.ac.uk")
+  time <-
+    as.POSIXct("2022-4-1 10:00:00", tz = "Europe/Helsinki")
+  pvol <- expect_s3_class(get_pvol("ukjer", time, param = "all"), "pvol")
+  expect_true(bioRad::is.pvol(pvol))
+  expect_identical(
+    lubridate::floor_date(pvol$datetime, "5 mins"),
+    lubridate::with_tz(time, "UTC")
+  )
+  pvolsp <- expect_s3_class(get_pvol("ukjer", pulse_type="sp", time, param = "all"), "pvol")
+  expect_true(bioRad::is.pvol(pvolsp))
+  expect_identical(
+    lubridate::floor_date(pvolsp$datetime, "5 mins"),
+    lubridate::with_tz(time, "UTC")
+  )
+  expect_false(identical(pvol, pvolsp))
+})
+
+test_that("plusetype argument",{
+  expect_error(get_pvol("ukcas", Sys.time(), pulse_type="ll"),"pulse_type")
+  expect_error(get_pvol("ukcas", Sys.time(), pulse_type=1),"pulse_type")
+})
+test_that("404 no data",{
+  skip_if_offline("ncas-radar-o.s3-ext.jc.rl.ac.uk")
+  expect_error(get_pvol("ukcas", as.POSIXct("2012-4-1 10:00:10")),class="getRad_error_uk_no_data_404")
+})
+
+test_that("no sp data",{
+  skip_if_offline("ncas-radar-o.s3-ext.jc.rl.ac.uk")
+  time <-
+    as.POSIXct("2022-4-1 10:05:10", tz = "Europe/Helsinki")
+  expect_error(get_pvol("ukcas", time, pulse_type="sp"),class="getRad_error_uk_no_sp_data")
+})
+
+
